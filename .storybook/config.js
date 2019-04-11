0f91e420e157e7747958bbcf04f5c1901838af14
Luke Mico <lukemico@me.com>
2019-04-11 22:10:45 +1000

    display jsx of React-storybook-story JSX addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,13 @@
-import {configure} from '@storybook/react'
+import { configure, setAddon } from '@storybook/react'
+import JSXAddon from 'storybook-addon-jsx';
+
+setAddon(JSXAddon);
 
 const req = require.context('../src', true, /.stories.js$/)
 
 function loadStories() {
- req.keys().forEach(file => req(file));   
+    require('./welcomeStory');
+    req.keys().forEach(file => req(file));   
 }
 
 configure(loadStories, module);
--- src/Button.stories.js
+++ src/Button.stories.js
@@ -3,6 +3,10 @@ import React from 'react';
 import {storeisOf, storiesOf} from '@storybook/react';
 import {Button} from './Button';
 
-storiesOf('Button', module).add('with background', () => (
-    <Button bg="palegoldenrod">Hello World</Button>
+storiesOf('Button', module)
+    .addWithJSX('with background', () => (
+    <Button bg="palegoldenrod">Hello World 1</Button>
 ))
+    .addWithJSX('with background 2', () => (
+    <Button bg="palegoldenrod">Hello World 2</Button>
+));
