8724f5e8c7363e7c9ea9cba8fbe5940dd45f205c
Luke Mico <lukemico@me.com>
2019-04-30 08:02:39 +1000

    install node_modules

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,12 @@
-import { configure, setAddon } from '@storybook/react'
+import { configure, setAddon, addDecorator } from '@storybook/react'
+import { withInfo } from '@storybook/addon-info';
 import JSXAddon from 'storybook-addon-jsx';
 
+addDecorator(
+    withInfo({
+      header: true, // Global configuration for the info addon across all of your stories.
+    })
+  ); 
 setAddon(JSXAddon);
 
 const req = require.context('../src', true, /.stories.js$/)
--- src/Button.stories.js
+++ src/Button.stories.js
@@ -1,13 +1,56 @@
 import React from 'react';
-import { withInfo } from '@storybook/addon-info';
+// import { wInfo } from './utils';
+// import { action } from '@storybook/addon-actions';
 
-import { storiesOf } from '@storybook/react';
-import { Button } from './Button';
 
-storiesOf('Button', module)
-    .addWithJSX('with background 1', () => (
-        <Button bg="palegoldenrod">Hello World 1</Button>
-    ))
-    .addWithJSX('with background 2', () => (
-        <Button bg="green">Hello World 2</Button>
-    ));
+// import { storiesOf, addDecorator } from '@storybook/react';
+// import { action } from '@storybook/addon-actions';
+// import { Button } from './Button';
+
+// storiesOf('Button', module)
+//     .addWithJSX(
+//         'with background 1', 
+//         wInfo(`description`
+//         ) 
+//         <Button bg="palegoldenrod">Hello World 1</Button>
+//     ))
+//     .addWithJSX('with background 2', () => (
+//         <Button bg="green">Hello World 2</Button>
+//     ));
+
+
+
+
+
+// storiesOf('Button', module).add('with text', () => (
+//     <Button onClick={action('clicked')}>Hello Button</Button>
+// ));
+
+
+import { storiesOf, addDecorator } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import Button from './Button';
+
+const styles = {
+  textAlign: 'center',
+};
+const CenterDecorator = storyFn => <div style={styles}>{storyFn()}</div>;
+addDecorator(CenterDecorator);
+
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+
+storiesOf('Button', module).add('with text', () => (
+    <Button onClick={action('clicked')}>Hello Button</Button>
+));
+
+// storiesOf('Button', module)
+//   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+//   .add('with some emojies', () => (
+//     <Button onClick={action('clicked')}>
+//       <span role="img" aria-label="so cool">
+//         😀 😎 👍 💯
+//       </span>
+//     </Button>
+//   ));
